9d1cb3852d7c3388f963ade81d85efdf60eaff3b
Alonso <alvergarax@gmail.com>
2026-08-31 15:06:47 -0400

    Creación de home: ruta, plantilla, vista

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import registro_usuario
+from .views import registro_usuario, HomeView
+
 urlpatterns = [
     path('registro/', registro_usuario, name='registro'),
+    path('', HomeView.as_view(), name='home')
 ]
+
+#as_view() transforma la clase HomeView en una función ejecutable para atender la petición HTTP.
--- core/views.py
+++ core/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from .forms import RegistroForm
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+# FBV vista de registro
 def registro_usuario(request):
+    #si el usuario completa y envia el formulario
     if request.method == 'POST':
         form = RegistroForm(request.POST)
         if form.is_valid():
             usuario = form.save()      # Guarda el usuario en la BD 
             login(request, usuario)    # Inicia sesión automáticamente
             return redirect('home')    # Redirige a la página principal
-    else: # metodo GET
+    #si llega a la pagina de registro (metodo GET)
+    else: 
         form = RegistroForm()
     
     return render(request, 'registro.html', {'form': form}) # sigue esta vía si el form.is_valid es false o si el metodo es GET
+
+
+# CBV vista de home con mixin
+class HomeView(LoginRequiredMixin, TemplateView):# Mixin: Solo los usuarios autenticados con una sesión válida pueden ver la página.
+    template_name= 'home.html'
