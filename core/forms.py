54b481e3bf1365f38badc921f1dd67d0308fed41
Alonso <alvergarax@gmail.com>
2026-08-31 19:10:35 -0400

    añade ProyectoForm y TareaForm con validación básica forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm #formulario base para registrar usuarios
 from django.contrib.auth.models import User #tabla usuario en BD
 from django import forms
+from .models import Proyecto, Tarea
 
 class RegistroForm(UserCreationForm):
     email = forms.EmailField(required= False)
@@ -8,3 +9,31 @@ class RegistroForm(UserCreationForm):
         model = User
         fields = ['username', 'email']
 
+class ProyectoForm(forms.ModelForm):
+    class Meta:
+        model = Proyecto
+        fields= ['nombre', ]
+        widgets ={
+            'nombre':forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre', '')
+        if len(nombre) <3:
+            raise forms.ValidationError('El nombre del proyecto debe tener al menos 3 caracteres')
+        return nombre
+
+class TareaForm(forms.ModelForm):
+    class Meta:
+        model = Tarea
+        fields = ['nombre', 'completada']
+        widgets={
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'completada': forms.CheckboxInput(attrs={'class': 'form-check-input'})
+        }
+
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre', '')
+        if len(nombre) <3:
+            raise forms.ValidationError('El nombre de la tarea debe tener al menos 3 caracteres')
+        return nombre
